215ce238c675dd0825be2a664e87cb268dc5f227
patrickwensel <pwensel@hotmail.com>
2016-04-10 20:10:05 -0400

    Upgarades

--- AMS/Global.asax.cs
+++ AMS/Global.asax.cs
@@ -6,8 +6,6 @@ using System.Web.Routing;
 
 namespace AMS
 {
-    // Note: For instructions on enabling IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=301868
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
